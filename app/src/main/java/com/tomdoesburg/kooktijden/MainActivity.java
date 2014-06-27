8446e55bfda09a81cac0ca01ed6c33fa5af85efa
TomDoes <t.doesburg@gmail.com>
2014-06-27 14:43:18 +0200

    Small fixes

--- app/src/main/java/com/tomdoesburg/kooktijden/MainActivity.java
+++ app/src/main/java/com/tomdoesburg/kooktijden/MainActivity.java
@@ -1,7 +1,6 @@
 package com.tomdoesburg.kooktijden;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,10 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-
-import com.tomdoesburg.model.Vegetable;
 import com.tomdoesburg.sqlite.MySQLiteHelper;
+import com.tomdoesburg.model.Vegetable;
 
 public class MainActivity extends Activity {
 
