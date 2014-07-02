65fd9fa9444adba60af43c9107a676daee7e7b6f
Joost <timmerman_joost@hotmail.com>
2014-07-02 22:26:46 +0200

    swipert om jouw type kookplaat te selecteren

--- app/src/main/java/com/tomdoesburg/kooktijden/MainActivity.java
+++ app/src/main/java/com/tomdoesburg/kooktijden/MainActivity.java
@@ -1,41 +1,45 @@
 package com.tomdoesburg.kooktijden;
 
-import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
+import android.view.View;
 
 import com.tomdoesburg.model.Vegetable;
 import com.tomdoesburg.sqlite.MySQLiteHelper;
 
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+    SharedPreferences sharedPrefs;
+
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    MyFragmentPagerAdapter mFragmentPagerAdapter;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //set correct interface
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        int cookertype = sharedPrefs.getInt("cookertype", 0);
-        switch (cookertype) {
-            case 1:
-                setContentView(R.layout.kookplaat1pits);
-                break;
-            case 2:
-                setContentView(R.layout.kookplaat2pits);
-                break;
-            default:
-                //no cooker type selected, set interface where user can select one
-                setContentView(R.layout.first_start_cookers);
-                ImageButton kookplaat1pits = (ImageButton)findViewById(R.id.kookplaat1pits);
-                ImageButton kookplaat2pits = (ImageButton)findViewById(R.id.kookplaat2pits);
-        }
+        //set default preferences
+        //PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
+        setContentView(R.layout.swipert);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mFragmentPagerAdapter);
+
+        //set the correct interface (if none set in preferences, this lets user choose one as default)
+        //setInterface(false);
 
         MySQLiteHelper db = new MySQLiteHelper(this);
 
@@ -68,4 +72,60 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    //sets the correct cooker type (called from kookplaten_overview.xml)
+    public void setKookplaatType(View view){
+
+        //save cooker type in settings
+        int type;
+        switch(view.getId()) {
+            case R.id.kookplaat1pits:
+                type=1;
+                break;
+            case R.id.kookplaat2pits:
+                type=2;
+                break;
+            case R.id.kookplaat3pits:
+                type=3;
+                break;
+            case R.id.kookplaat4pits:
+                type=4;
+                break;
+            case R.id.kookplaat5pits:
+                type=5;
+                break;
+            case R.id.kookplaat6pits:
+                type=6;
+                break;
+            default:
+                throw new RuntimeException("Unknown button ID");
+        }
+        sharedPrefs.edit().putInt("aantalPitten",type).commit();
+
+        //set the correct interface
+        setInterface(false);
+    }
+
+    //sets the interface for a certain cooker
+    public void setInterface(boolean reset){
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int aantalPitten = sharedPrefs.getInt("aantalPitten", 0);
+
+        //reset current value, so that user must select new cooker type
+        if(reset){
+            aantalPitten = 0;
+        }
+
+        //interface switch
+        switch (aantalPitten) {
+            case 1:
+                setContentView(R.layout.kookplaat1pits);
+                break;
+            case 2:
+                setContentView(R.layout.kookplaat2pits);
+                break;
+            default:
+                //no cooker type selected, set interface where user can select one
+                setContentView(R.layout.kookplaten_overview);
+        }
+    }
 }
--- app/src/main/java/com/tomdoesburg/sqlite/MySQLiteHelper.java
+++ app/src/main/java/com/tomdoesburg/sqlite/MySQLiteHelper.java
@@ -1,10 +1,10 @@
 package com.tomdoesburg.sqlite;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.content.Context;
 import android.util.Log;
 
 import com.tomdoesburg.model.Vegetable;
