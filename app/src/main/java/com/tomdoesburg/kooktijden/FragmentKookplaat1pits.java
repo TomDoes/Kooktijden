7c5f36b64a582f9ffc88ed6a61589e59844eae5c
Joost <timmerman_joost@hotmail.com>
2014-07-03 22:12:50 +0200

    swipert om jouw type kookplaat te selecteren

---
+++ app/src/main/java/com/tomdoesburg/kooktijden/FragmentKookplaat1pits.java
@@ -0,0 +1,23 @@
+package com.tomdoesburg.kooktijden;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Joost on 2-7-2014.
+ */
+// Instances of this class are fragments representing a single
+// object in our collection.
+public class FragmentKookplaat1pits extends Fragment {
+    public static final String ARG_OBJECT = "object";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        // The last two arguments ensure LayoutParams are inflated properly.
+        View rootView = inflater.inflate(R.layout.kookplaat1pits, container, false);
+        return rootView;
+    }
+}
--- app/src/main/java/com/tomdoesburg/kooktijden/FragmentKookplaat2pits.java
+++ app/src/main/java/com/tomdoesburg/kooktijden/FragmentKookplaat2pits.java
@@ -1,27 +1,23 @@
 package com.tomdoesburg.kooktijden;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.support.v4.app.Fragment;
 
 /**
  * Created by Joost on 2-7-2014.
  */
 // Instances of this class are fragments representing a single
 // object in our collection.
-public class DemoFragment extends Fragment {
+public class FragmentKookplaat2pits extends Fragment {
     public static final String ARG_OBJECT = "object";
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        // The last two arguments ensure LayoutParams are inflated
-        // properly.
-        View rootView = inflater.inflate(R.layout.kookplaat1pits, container, false);
-        Bundle args = getArguments();
-        ((TextView) rootView.findViewById(android.R.id.text1)).setText(Integer.toString(args.getInt(ARG_OBJECT)));
+        // The last two arguments ensure LayoutParams are inflated properly.
+        View rootView = inflater.inflate(R.layout.kookplaat2pits, container, false);
         return rootView;
     }
 }
