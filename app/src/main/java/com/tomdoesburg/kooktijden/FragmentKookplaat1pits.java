a497520033d9170c17a9fcd2c90ec5e0e447220a
Joost <timmerman_joost@hotmail.com>
2014-07-11 11:59:46 +0200

    fixed: reageren op kookplaat-klik

--- app/src/main/java/com/tomdoesburg/kooktijden/FragmentKookplaat1pits.java
+++ app/src/main/java/com/tomdoesburg/kooktijden/FragmentKookplaat1pits.java
@@ -1,10 +1,13 @@
 package com.tomdoesburg.kooktijden;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by Joost on 2-7-2014.
@@ -13,6 +16,11 @@ import android.view.ViewGroup;
 // object in our collection.
 public class FragmentKookplaat1pits extends Fragment {
     public static final String ARG_OBJECT = "object";
+    ProgressBar kookplaat1;
+    TextView kookplaat1text;
+    TimerHelper timerHelper;
+    int timeSeconds;
+    boolean timerRunning;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
@@ -20,4 +28,17 @@ public class FragmentKookplaat1pits extends Fragment {
         View rootView = inflater.inflate(R.layout.kookplaat1pits, container, false);
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //kookplaat 1
+        ProgressBar kookplaat1 = (ProgressBar) getView().findViewById(R.id.kookplaat1);
+        TextView kookplaat1text = (TextView) getView().findViewById(R.id.kookplaat1text);
+        TimerHelper timerHelper = new TimerHelper();
+        timerHelper.init(kookplaat1, kookplaat1text);
+
+    }
+
 }
--- app/src/main/java/com/tomdoesburg/kooktijden/MainActivity.java
+++ app/src/main/java/com/tomdoesburg/kooktijden/MainActivity.java
@@ -2,14 +2,10 @@ package com.tomdoesburg.kooktijden;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.tomdoesburg.model.Vegetable;
 import com.tomdoesburg.sqlite.MySQLiteHelper;
@@ -40,9 +36,6 @@ public class MainActivity extends FragmentActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mFragmentPagerAdapter);
 
-        //set the correct interface (if none set in preferences, this lets user choose one as default)
-        //setInterface(false);
-
         MySQLiteHelper db = new MySQLiteHelper(this);
 
         db.addVegetable(new Vegetable("Wortel", 600, "Een wortel is oranje en heel gezond!"));
@@ -72,71 +65,6 @@ public class MainActivity extends FragmentActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-
-    public void setTimer(View v) {
-
-        final ProgressBar kookplaat;
-        final TextView kookplaattext;
-        int id;
-
-        switch (v.getId()) {
-            case R.id.kookplaat1:
-                kookplaat = (ProgressBar) findViewById(R.id.kookplaat1);
-                kookplaattext = (TextView) findViewById(R.id.kookplaat1text);
-                break;
-            case R.id.kookplaat2:
-                kookplaat = (ProgressBar) findViewById(R.id.kookplaat2);
-                kookplaattext = (TextView) findViewById(R.id.kookplaat2text);
-                break;
-            case R.id.kookplaat3:
-                kookplaat = (ProgressBar) findViewById(R.id.kookplaat3);
-                kookplaattext = (TextView) findViewById(R.id.kookplaat3text);
-                break;
-            default:
-                throw new RuntimeException("Unknown kookplaat ID");
-        }
-
-        //TODO
-        //create a selection thingy to select which vegetable to put on this timer
-        //int vegetableID = vegetablePicker();
-        //MySQLiteHelper db = new MySQLiteHelper(this);
-        //int timeSeconds = db.getVegetable(vegetableID).getCookingTime();
-        int timeSeconds = 10;
-
-        //something to initialize a timer on a given kookplaat for a given vegetable (separate thread using handler?)
-        //something to start the actual timer when the water starts boiling
-        initOrStart(kookplaat, kookplaattext, timeSeconds);
-
-    }
-
-    void initOrStart(final ProgressBar kookplaat, final TextView kookplaattext, final int timeSeconds) {
-
-        kookplaat.setMax(timeSeconds);
-
-        CountDownTimer timer = new CountDownTimer(timeSeconds * 1000, 250) {
-            // 500 means, onTick function will be called at every 500 milliseconds
-
-            @Override
-            public void onTick(long leftTimeInMilliseconds) {
-                long seconds = leftTimeInMilliseconds / 1000;
-                int barVal = (timeSeconds) - ((int) (seconds / 60 * 100) + (int) (seconds % 60));
-                kookplaat.setProgress(barVal);
-                kookplaattext.setText(String.format("%02d", seconds / 60) + ":" + String.format("%02d", seconds % 60));
-                // format the textview to show the easily readable format
-
-            }
-
-            @Override
-            public void onFinish() {
-                if (kookplaattext.getText().equals("00:00")) {
-                    kookplaattext.setText("STOP");
-                } else {
-                    kookplaattext.setText("+1:00");
-                }
-            }
-        }.start();
     }
 
 }
